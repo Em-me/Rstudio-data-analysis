847879eec108625cfad9889dbb901ef34e067122
Em_me <ngahuemily76@gmail.com>
2023-03-23 00:05:00 +0300

    review of basic calculations

---
+++ day two.R
@@ -0,0 +1,47 @@
+4+5
+#exponents 
+5^2
+5**2-#5 raised to 2 
+division 
+10/2
+#Euclidean division -gives the remainder 
+10%/%2
+24%/%5
+24%%5
+#some special numerical constants 
+#inf=infinite,+ve,-ve
+#NaN= not a number 
+#NA = missing value
+2.4/3
+29%%5
+#in 29%%5,5 is the euclidean divisor and 4 is the euclidean division remeinder 
+#floating point division 29/5
+29/9
+#month.abb-abbreviation of first 3 letters
+#month.name
+LETTERS
+LETTERS[17]
+letters
+letters[17]
+#imaginary numbers 
+sqrt(-1)#NaN
+sqrt(-1+0i)
+#log 
+log(5)-#natural logs 
+log10(5)-#log to base 10
+exp(1)
+#trigonometric functions in radians ,default
+degrees<-60
+radians<- degrees *( pi / 180)
+radians
+sin(radians)
+10-3*2
+#operator precedence 
+#-exponents and roots 
+#-multiplication and division 
+#-addition and subtruction 
+(10-3)*2
+dir()-#on the hard drive 
+ls()-# in your session 
+?"+"
+??obesity
