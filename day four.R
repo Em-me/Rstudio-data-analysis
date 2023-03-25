4a0498b55d9ba35c6c2701124e113319e516e184
Em_me <ngahuemily76@gmail.com>
2023-03-25 22:27:34 +0300

    if statements

---
+++ day four.R
@@ -0,0 +1,24 @@
+#if
+exams<-35
+if (exams <=30){
+  print("fail")
+}else if (exams<=40){
+  print("You have a Retake")
+}else{
+  print("Pass")
+}
+quantity <-  25
+# Set the is-else statement
+if (quantity > 20) {
+  print('You sold a lot!')
+} else {
+  print('Not enough for today')  
+}
+
+x<-c(3,2,2,3,1,0)
+x
+if (all(x<5)){
+  print("All the numbers in x are smalller than five")
+}
+x
+
